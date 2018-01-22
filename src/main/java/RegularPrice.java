14de93d680ec16030e5813cb9a25b1c24981d717
Christian Wiedehöft <wiedehoeft.christian@gmail.com>
2018-01-22 08:22:22 +0000

    Unexpected red test

--- src/main/java/RegularPrice.java
+++ src/main/java/RegularPrice.java
@@ -1,4 +1,15 @@
-package PACKAGE_NAME;
-
 public class RegularPrice {
+
+    private static final Euro BASE_PRICE = new Euro(1.50);
+    private static final Euro PRICE_PER_DAY = new Euro(1.50);
+    private static final int DAYS_DISCOUNTED = 3;
+
+    public Euro getCharge(int daysRented) {
+
+        if (daysRented <= DAYS_DISCOUNTED) {
+            return BASE_PRICE;
+        }
+
+        return BASE_PRICE.add(PRICE_PER_DAY);
+    }
 }
--- src/test/java/RegularPriceTest.java
+++ src/test/java/RegularPriceTest.java
@@ -1,4 +1,23 @@
-package PACKAGE_NAME;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
 
 public class RegularPriceTest {
+
+    private RegularPrice price;
+
+    @Before
+    public void setUp() throws Exception {
+        price = new RegularPrice();
+    }
+
+    @Test
+    public void testChargingOneDayRental() throws Exception {
+        Assert.assertEquals(new Euro(1.50), price.getCharge(1));
+    }
+
+    @Test
+    public void testChargingFourDayRental() throws Exception {
+        Assert.assertEquals(new Euro(3.00), price.getCharge(4));
+    }
 }
