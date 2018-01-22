b5d626897f86dc88e567c6e6e6bcda53e98e2bad
Christian Wiedehöft <wiedehoeft.christian@gmail.com>
2018-01-22 14:04:31 +0000

    move tests

--- src/test/java/MovieTest.java
+++ src/test/java/MovieTest.java
@@ -5,14 +5,7 @@ import static org.junit.Assert.assertEquals;
 public class MovieTest {
 
     @Test
-    public void testBasePrice() throws Exception {
-        assertEquals(new Euro(2.00), Movie.getCharge(1));
-        assertEquals(new Euro(2.00), Movie.getCharge(1));
-    }
-
-    @Test
-    public void testPricePerPay() throws Exception {
+    public void usingNewReleasePrice() throws Exception {
         assertEquals(new Euro(3.75), Movie.getCharge(3));
-        assertEquals(new Euro(5.50), Movie.getCharge(4));
     }
 }
--- src/test/java/NewReleasePriceTest.java
+++ src/test/java/NewReleasePriceTest.java
@@ -1,4 +1,18 @@
-package PACKAGE_NAME;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
 
 public class NewReleasePriceTest {
+
+    @Test
+    public void testBasePrice() throws Exception {
+        assertEquals(new Euro(2.00), Movie.getCharge(1));
+        assertEquals(new Euro(2.00), Movie.getCharge(1));
+    }
+
+    @Test
+    public void testPricePerPay() throws Exception {
+        assertEquals(new Euro(3.75), Movie.getCharge(3));
+        assertEquals(new Euro(5.50), Movie.getCharge(4));
+    }
 }
