f1f2e4bfdde65598c238b107ccbef3e180303585
Christian Wiedehöft <wiedehoeft.christian@gmail.com>
2018-01-22 14:01:21 +0000

    Split classes: move method to new class

---
+++ src/main/java/NewReleasePrice.java
@@ -0,0 +1,15 @@
+public class NewReleasePrice {
+
+    private static final Euro BASE_PRICE = new Euro(2.00);
+    private static final Euro PRICE_PER_DAY = new Euro(1.75);
+    private static final int DAYS_DISCOUNTED = 2;
+
+    public static Euro tmpCharge(int daysRented) {
+        Euro result = BASE_PRICE;
+        if (daysRented > DAYS_DISCOUNTED) {
+            int additionalDays = daysRented - DAYS_DISCOUNTED;
+            result = result.plus(PRICE_PER_DAY.times(additionalDays));
+        }
+        return result;
+    }
+}
