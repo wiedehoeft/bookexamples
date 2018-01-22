e2f15cc17b1c66acef100fe91bde4812f483933a
Christian Wiedehöft <wiedehoeft.christian@gmail.com>
2018-01-22 13:45:26 +0000

    Example for interface evolution: refactor tests

--- src/test/java/MovieTest.java
+++ src/test/java/MovieTest.java
@@ -6,13 +6,13 @@ public class MovieTest {
 
     @Test
     public void testBasePrice() throws Exception {
-        assertEquals(2.00, Movie.getCharge(1).getAmount(), 0.01);
-        assertEquals(2.00, Movie.getCharge(1).getAmount(), 0.01);
+        assertEquals(new Euro(2.00), Movie.getCharge(1));
+        assertEquals(new Euro(2.00), Movie.getCharge(1));
     }
 
     @Test
     public void testPricePerPay() throws Exception {
-        assertEquals(3.75, Movie.getCharge(3).getAmount(), 0.01);
-        assertEquals(5.50, Movie.getCharge(4).getAmount(), 0.01);
+        assertEquals(new Euro(3.75), Movie.getCharge(3));
+        assertEquals(new Euro(5.50), Movie.getCharge(4));
     }
 }
