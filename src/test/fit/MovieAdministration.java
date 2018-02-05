eb61e6507e1333e0e3e8625a1ec248ca23f21be0
Christian <wiedehoeft.christian@gmail.com>
2018-02-05 16:01:34 +0100

    Added first failing action fit-test

---
+++ src/test/fit/MovieAdministration.java
@@ -0,0 +1,24 @@
+import fit.Fixture;
+
+public class MovieAdministration extends Fixture{
+
+  public void newMovie() {
+
+  }
+
+  public void save() {
+
+  }
+
+  public int movieNumber() {
+    return 0;
+  }
+
+  public void movieTitle(String title) {
+
+  }
+
+  public void priceCategory(String category) {
+
+  }
+}
--- src/test/fit/runFit.sh
+++ src/test/fit/runFit.sh
@@ -1,4 +1,5 @@
 export CLASSPATH=./fit.jar:../../../build/libs/bookexamples-1.0-SNAPSHOT.jar
 javac Pricing.java
+javac MovieAdministration.java
 export CLASSPATH=./fit.jar:../fit:../../../build/libs/bookexamples-1.0-SNAPSHOT.jar
 java fit.FileRunner alltests.html alltests-result.html
