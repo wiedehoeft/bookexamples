812417ca6f222ac6d9e07d40fb53c9be8cc95932
Christian <wiedehoeft.christian@gmail.com>
2018-02-05 15:28:45 +0100

    Added first failing tests

---
+++ src/test/fit/Pricing.java
@@ -0,0 +1,11 @@
+import fit.ColumnFixture;
+import videostore.Price;
+
+public class Pricing extends ColumnFixture {
+
+  public int daysRented;
+
+  public double regularPrice() {
+    return Price;
+  }
+}
