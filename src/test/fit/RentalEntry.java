b2cb65c687881d563845d6b23de8a6fe267cfce2
Christian <wiedehoeft.christian@gmail.com>
2018-02-06 15:18:55 +0100

    changed rentalentry tests to use arrayadapter

--- src/test/fit/RentalEntry.java
+++ src/test/fit/RentalEntry.java
@@ -1,4 +1,5 @@
 import fit.ColumnFixture;
+import videostore.Euro;
 import videostore.Rental;
 import videostore.UnknownMovieException;
 import videostore.VideoStore;
@@ -7,11 +8,24 @@ public class RentalEntry extends ColumnFixture {
 
   private VideoStore store = SystemUnderTest.instance();
 
+  public int[] daysRented;
   public int movieNumber;
-  public int daysRented;
 
-  public double charge() throws UnknownMovieException {
-    Rental rental = store.addRental(movieNumber, daysRented);
-    return rental.getCharge().getAmount();
+  public Euro[] charges() throws UnknownMovieException {
+    Euro[] result = new Euro[daysRented.length];
+
+    for (int i = 0; i < daysRented.length; i++) {
+      Rental rental = store.addRental(movieNumber, daysRented[i]);
+      result[i] = rental.getCharge();
+    }
+
+    return result;
+  }
+
+  public Object parse(String text, Class type) throws Exception {
+    if (Euro.class.equals(type)) {
+      return Euro.parse(text);
+    }
+    return super.parse(text, type);
   }
 }
